99dbcbb25e70af29e0c814e9a570bd27e87b9ba9
ILIE VILCEANU <71116646+iivilceanu@users.noreply.github.com>
2022-04-28 17:38:48 +0300

    Create break_all_container_blob_lease.ps1

---
+++ break_all_container_blob_lease.ps1
@@ -0,0 +1,11 @@
+# create a context 
+$Context = New-AzStorageContext -StorageAccountName "yourstorageaccount" -StorageAccountKey "yourstorageaccountkey"
+
+#set your container
+$Container = "yourcontainername"
+
+#Get a reference to all the blobs in the container.
+$blobs = Get-AzStorageBlob -Container $Container -Context $Context
+
+#Remove lease on each Blob
+$blobs | %{$_.ICloudBlob.BreakLease()}
