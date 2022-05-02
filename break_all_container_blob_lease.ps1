792d70bba0b0a7448d017100b7778df4d14b7d62
ILIE VILCEANU <71116646+iivilceanu@users.noreply.github.com>
2022-05-02 11:08:53 +0300

    Update break_all_container_blob_lease.ps1

--- break_all_container_blob_lease.ps1
+++ break_all_container_blob_lease.ps1
@@ -1,3 +1,21 @@
+#Powershell version
+$PSVersionTable.PSVersion
+
+#Import module AZ
+Install-Module -Name Az
+
+#Check if the module AZ is installed
+Get-InstalledModule -Name Az
+
+
+#Login with an Azure AD credential that has either storage account owner or contributer Azure role assignment
+Connect-AzAccount
+
+#Get subscription
+Get-AzSubscription 
+# Set Subscription
+Select-AzSubscription -subscriptionid yoursubscriptionid
+
 # create a context 
 $Context = New-AzStorageContext -StorageAccountName "yourstorageaccount" -StorageAccountKey "yourstorageaccountkey"
 
